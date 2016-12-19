ee8cc6eabcbc0e6e7547db0247003b63f6a2f672
Ryan Catalani <catalani.ryan@gmail.com>
2016-12-18 22:23:08 -1000

    Email attaches PDF, and rake methods accept email address.

--- Rakefile
+++ Rakefile
@@ -1,10 +1,10 @@
 require_relative 'search'
 
-task :search, [:search_name, :search_url, :search_text] do |t, args|
-	search(args.search_name, args.search_url, args.search_text)
+task :search, [:search_name, :search_url, :search_text, :email] do |t, args|
+	search(args.search_name, args.search_url, args.search_text, args.email)
 end
 
-task :get_url_and_search, [:search_name, :from_url, :search_text] do |t, args|
+task :get_url_and_search, [:search_name, :from_url, :search_text, :email] do |t, args|
 	search_url = get_pdf_url(args.from_url)
-	search(args.search_name, search_url, args.search_text)
+	search(args.search_name, search_url, args.search_text, args.email)
 end
--- search.rb
+++ search.rb
@@ -11,30 +11,34 @@ def get_pdf_url(from_url)
 	return pdf_urls.first
 end
 
-def search(search_name, search_url, search_text)
+def search(search_name, search_url, search_text, email)
 
 	pdf_tempfile = Down.download(search_url)
 
 	begin
 		File.open(pdf_tempfile, 'rb') do |io|
 			reader = PDF::Reader.new(io)
-
 			text = reader.pages.map(&:text).join('').downcase
-			if text.include?(search_text.downcase)
-				
+
+			if text.include?(search_text.downcase)	
 				subj_date = Time.now.strftime('%b %e %Y')
 				file_date = Time.now.strftime('%Y%m%d')
 				file_name = search_name.gsub(/\W/,'_').downcase + "_#{file_date}"
 
 				mg_client = Mailgun::Client.new ENV['MAILGUN_API_KEY']
 
-				message_params =  { from: ENV['EMAIL'],
-				                    to:   ENV['EMAIL'],
-				                    subject: "#{search_name} search found",
-				                    text:    "Text '#{search_text}' found in PDF. Link: #{search_url}"			                  }
+				mb = Mailgun::MessageBuilder.new
+				mb.from(email)
+				mb.add_recipient(:to, email)
+				mb.subject("#{search_name} search found: #{subj_date}")
+				mb.body_text("Text '#{search_text}' found in PDF. Link: #{search_url}")
 
-				mg_client.send_message ENV['MAILGUN_DOMAIN'], message_params
+				io.rewind
+				mb.add_attachment(pdf_tempfile, "#{file_name}.pdf")
+
+				mg_client.send_message ENV['MAILGUN_DOMAIN'], mb
 			end
+
 		end
 	ensure
 		pdf_tempfile.close
