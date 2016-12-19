401825e0f2f2cdee975bfeae573d672b26486186
Ryan Catalani <catalani.ryan@gmail.com>
2016-12-18 21:42:06 -1000

    Use down gem for downloads and better file handling.

--- search.rb
+++ search.rb
@@ -1,33 +1,35 @@
-require 'open-uri'
+require 'down'
 require 'pdf-reader'
 require 'mailgun'
 
 def search(search_name, search_url, search_text)
 
-	io = open(search_url)
+	pdf_tempfile = Down.download(search_url)
+
 	begin
-		reader = PDF::Reader.new(io)
+		File.open(pdf_tempfile, 'rb') do |io|
+			reader = PDF::Reader.new(io)
 
-		text = reader.pages.map(&:text).join('').downcase
-		if text.include?(search_text.downcase)
-			
-			subj_date = Time.now.strftime('%b %e %Y')
-			file_date = Time.now.strftime('%Y%m%d')
-			file_name = search_name.gsub(/\W/,'_').downcase + "_#{file_date}"
+			text = reader.pages.map(&:text).join('').downcase
+			if text.include?(search_text.downcase)
+				
+				subj_date = Time.now.strftime('%b %e %Y')
+				file_date = Time.now.strftime('%Y%m%d')
+				file_name = search_name.gsub(/\W/,'_').downcase + "_#{file_date}"
 
-			mg_client = Mailgun::Client.new ENV['MAILGUN_API_KEY']
+				mg_client = Mailgun::Client.new ENV['MAILGUN_API_KEY']
 
-			message_params =  { from: ENV['EMAIL'],
-			                    to:   ENV['EMAIL'],
-			                    subject: "#{search_name} search found",
-			                    text:    "Text '#{search_text}' found in PDF. Link: #{search_url}"			                  }
+				message_params =  { from: ENV['EMAIL'],
+				                    to:   ENV['EMAIL'],
+				                    subject: "#{search_name} search found",
+				                    text:    "Text '#{search_text}' found in PDF. Link: #{search_url}"			                  }
 
-			mg_client.send_message ENV['MAILGUN_DOMAIN'], message_params
-
+				mg_client.send_message ENV['MAILGUN_DOMAIN'], message_params
+			end
 		end
 	ensure
-		io.close
-		io.unlink
+		pdf_tempfile.close
+		pdf_tempfile.unlink
 	end
 
 end
